e87e74914f30b714f3efdea5c52f3316237ce17e
adarshsuresh07 <adarshsuresh@cet.ac.in>
2020-05-22 18:30:55 +0530

    slight css changes

--- src/Components/Discussion.jsx
+++ src/Components/Discussion.jsx
@@ -48,23 +48,25 @@ export default class Discussion extends Component {
   render() {
     if (this.state.isOpen) {
       return (
-        <div className="discussion-container">
+        <div className="discussion-tab">
           <div onClick={this.toggle} className="discussion-title">
             Discuss
           </div>
-          <div className="thought-content">
-            {this.state.thought ? this.state.thought.attrs.text : null}<br />
-            <small>{this.state.thought ? "@" + this.state.thought.attrs.author : null}</small>
-          </div>
-          <div className="discussions">{this.getDiscussions()}</div>
-          <div className="discussion-new">
-            <Newmessage {...this.props} updateDiscussion={this.updateDiscussion} />
+          <div className="discussion-container">
+            <div className="thought-content">
+              {this.state.thought ? this.state.thought.attrs.text : null}<br />
+              <small>{this.state.thought ? "@" + this.state.thought.attrs.author : null}</small>
+            </div>
+            <div className="discussions">{this.getDiscussions()}</div>
+            <div className="discussion-new">
+              <Newmessage {...this.props} updateDiscussion={this.updateDiscussion} />
+            </div>
           </div>
         </div>
       );
     } else {
       return (
-        <div className="discussion-container collapsed">
+        <div className="discussion-tab collapsed">
           <div className="discussion-title" onClick={this.toggle}>
             Discuss
           </div>
@@ -73,62 +75,3 @@ export default class Discussion extends Component {
     }
   }
 }
-
-// class Discussion extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       loaddiscussion: false,
-//       discussions: [],
-//     };
-//   }
-
-//   streamCallback = (post) => {
-//     console.log(post);
-//   };
-
-//   async componentDidMount() {
-//     this.props.setload();
-
-//     Message.addStreamListener(this.streamCallback);
-
-//     const messages = await Message.fetchList({
-//       postid: this.props.id,
-//     });
-//     this.setState({ discussions: messages });
-//     localStorage.setItem("discussions", JSON.stringify(messages));
-//     this.setState({ loaddiscussion: true });
-//     this.props.setload();
-//   }
-
-//   componentWillUnmount() {
-//     Message.removeStreamListener(this.streamCallback);
-//   }
-
-//   getDiscussions = () => {
-//     // var discussions = JSON.parse(localStorage.getItem("discussions"));
-//     // if(!discussions) discussions=[];
-//     return this.state.discussions.map((item) => {
-//       return <DiscussionPost data={item} />;
-//     });
-//   };
-
-//   updateDiscussion = (messages) => {
-//     this.setState({ discussions: messages });
-//   };
-
-//   render() {
-//     return (
-//       <div className="discussion-container">
-//         {this.state.loaddiscussion ? this.getDiscussions() : null}
-//         <Newmessage {...this.props} updateDiscussion={this.updateDiscussion} />
-//         {/* <div className="discussion-inp-container">
-//           <input type="text" className="discussion-inp" />
-//           <button className="discussion-post-btn">Post</button>
-//         </div> */}
-//       </div>
-//     );
-//   }
-// }
-//
-// export default Discussion;
