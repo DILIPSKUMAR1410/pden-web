81e2dadd074387413cdbc6febf3967c3853ca8b9
rwithik <thoriumnitride@gmail.com>
2020-05-31 19:57:29 +0530

    mybook component

---
+++ src/Components/MyBook.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./MyBook.css";
+import { Newfeed, Post } from "./";
+
+export default class MyBook extends Component {
+  getThoughts = () => {
+    const data = [
+      {
+        attrs: {
+          text: "Text",
+          author: "Author",
+          date: new Date(),
+        },
+      },
+    ];
+    return data.map((thought) => {
+      return <Post data={thought} showDiscuss={this.props.showDiscuss} />;
+    });
+  };
+
+  render() {
+    return (
+      <div className="feed-container">
+        <div className="feed-full">
+          <div className="feedc">{this.getThoughts()}</div>
+        </div>
+      </div>
+    );
+  }
+}
